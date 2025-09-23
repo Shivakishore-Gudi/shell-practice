761ee8ee6b3a7afdc07351c4f5ffcab37bdf5b58
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-23 14:30:47 +0530

    some message

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "ramesh:: Hi suresh,How are you?"
+echo "suresh::hello ramesh iam fine, how are you?"
+echo "ramesh::what are you learning recently?"
+echo "suresh:: iam learning shel scripting ramesh"
