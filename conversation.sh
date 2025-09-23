be57e66ec9814dda6d3e3282ae6007d0e5b86eb4
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-23 14:54:54 +0530

    changed names

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-echo "ramesh:: Hi suresh,How are you?"
-echo "suresh::hello ramesh iam fine, how are you?"
-echo "ramesh::what are you learning recently?"
-echo "suresh:: iam learning shel scripting ramesh"
+PERSON1 = John;
+PERSON2 = Abraham;
+
+echo "$PERSON1:: Hi $PERSON2,How are you?"
+echo "$PERSON2::hello $PERSON1 iam fine, how are you?"
+echo "$PERSON1::what are you learning recently?"
+echo "$PERSON2:: iam learning shel scripting $PERSON1"
