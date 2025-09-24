dfc5d34ac6a22d509af07f609576ddfce544691a
shivakishore-gudi <shivakishoregudi8@gmail.com>
2025-09-24 16:10:24 +0530

    updates

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,7 @@
 echo "please enter the number:"
 read Number
 
-if [ $(($Number%2) -eq 0) ]; then
+if [ $(($Number%2)) -eq 0 ]; then
     echo "Given number $Number is Even"
 else
     echo "Given number $Number is Odd"
